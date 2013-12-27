ec0a9d0b246aa41dbb08ce95a9a76a09d0b76825
Martin Evans <martindevans@gmail.com>
2013-12-27 00:07:51 +0000

    Initial commit
    Signed-off-by: Martin Evans <martindevans@gmail.com>

---
+++ Nock/Program.fs
@@ -0,0 +1,55 @@
+﻿//http://www.urbit.org/2013/11/18/urbit-is-easy-ch2.html
+
+namespace Nock
+module Nock =
+
+    type noun = 
+        | Atom of int
+        | Cell of noun * noun
+
+    let wut noun =
+        match noun with
+        | Cell(a, b) -> Atom(0)                                                                 // ?[a b]           0
+        | Atom(a) -> Atom(1)                                                                    // ?a               1
+
+    let lus noun =
+        match noun with
+        | Atom(a) -> Atom(1 + a)                                                                // +a               1 + a
+        | Cell(_, _) -> raise (System.ArgumentException("Cannot lus a Cell"))
+
+    let tis noun =
+        match noun with
+        | Cell(a, b) -> Atom(if a = b then 0 else 1)                                            // =[a a]           0
+                                                                                                // =[a b]           1
+        | Atom(_) -> raise (System.ArgumentException("Cannot tis an Atom"))
+
+    let rec fas noun =
+        match noun with
+        | Cell (Atom(1), a) -> a                                                                // /[1 a]           a
+        | Cell (Atom(2), Cell(a, b)) -> a                                                       // /[2 a b]         a
+        | Cell (Atom(3), Cell(a, b)) -> b                                                       // /[3 a b]         b
+        | Cell (Atom(a), b) -> 
+            if a % 2 = 0 then
+                fas(Cell(Atom(2), fas(Cell(Atom(a / 2), b))))                                   // /[(a + a) b]     /[2 /[a b]]
+            else
+                fas(Cell(Atom(3), fas(Cell(Atom((a - 1) / 2), b))))                             // /[(a + a + 1) b] /[3 /[a b]]
+        | _ -> raise (System.ArgumentException("Invalid fas"))
+
+    let rec tar noun = 
+        match noun with
+        | Cell (a, Cell(Cell(b, c), d)) -> Cell(tar(Cell(a, Cell(b, c))), tar(Cell(a, d)))      // *[a [b c] d]     [*[a b c] *[a d]]
+        | Cell (a, Cell(Atom(0), b)) -> fas(Cell(b, a))                                         // *[a 0 b]         /[b a]
+        | Cell (a, Cell(Atom(1), b)) -> b                                                       // *[a 1 b]         b
+        | Cell (a, Cell(Atom(2), Cell(b, c))) -> tar(Cell(tar(Cell(a, b)), tar(Cell(a, c))))    // *[a 2 b c]       *[*[a b] *[a c]]
+        | Cell (a, Cell(Atom(3), b)) -> wut(tar(Cell(a, b)))                                    // *[a 3 b]         ?*[a b]
+        | Cell (a, Cell(Atom(4), b)) -> lus(tar(Cell(a, b)))                                    // *[a 4 b]         +*[a b]
+        | Cell (a, Cell(Atom(5), b)) -> tis(tar(Cell(a, b)))                                    // *[a 5 b]         =*[a b]
+        | Cell (a, Cell(Atom(6), Cell(b, Cell(c, d)))) -> tar(Cell(a, Cell(Atom(2), Cell(Cell(Atom(0), Atom(1)), Cell(Atom(2), Cell(Cell(Atom(1), Cell(c, d)), Cell(Cell(Atom(1), Atom(0)), Cell(Atom(2), Cell(Cell(Atom(1), Cell(Atom(2), Atom(3))), Cell(Cell(Atom(1), Atom(0)), Cell(Atom(4), Cell(Atom(4), b))))))))))))     // *[a 6 b c d]     *[a 2 [0 1] 2 [1 c d] [1 0] 2 [1 2 3] [1 0] 4 4 b]
+        | Cell (a, Cell(Atom(7), Cell(b, c))) -> tar(Cell(a, Cell(Atom(2), Cell(b, Cell(Atom(1), c)))))                                                            // *[a 7 b c]       *[a 2 b 1 c]
+        | Cell (a, Cell(Atom(8), Cell(b, c))) -> tar(Cell(a, Cell(Atom(7), Cell(Cell(Cell(Atom(7), Cell(Cell(Atom(0), Atom(1)), b)), Cell(Atom(0), Atom(1))), c))))       // *[a 8 b c]       *[a 7 [[7 [0 1] b] 0 1] c]                                                          //<===========
+        | Cell (a, Cell(Atom(9), Cell(b, c))) -> tar(Cell(a, Cell(Atom(7), Cell(c, Cell(Atom(2), Cell(Cell(Atom(0), Atom(1)), Cell(Atom(0), b)))))))               // *[a 7 c 2 [0 1] 0 b]
+        | Cell (a, Cell(Atom(10), Cell(Cell(b, c), d))) -> tar(Cell(a, Cell(Atom(8), Cell(c, Cell(Atom(7), Cell(Cell(Atom(0), Atom(3)), d))))))                    // *[a 10 [b c] d]  *[a 8 c 7 [0 3] d]
+        | Cell (a, Cell(Atom(10), Cell(b, c))) -> tar(Cell(a, c))                                                                                                  // *[a 10 b c]      *[a c]
+
+        | Atom a -> raise (System.ArgumentException("Cannot nock an Atom"))                     // *a               *a
+        | _ -> raise (System.ArgumentException("Invalid tar"))
---
+++ NockTest/UnitTest1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using Microsoft.FSharp.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nn = Nock.Nock.noun;
+
+namespace NockTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        #region noun factories
+        private static Nock.Nock.noun N(int i)
+        {
+            return Nock.Nock.noun.NewAtom(i);
+        }
+
+        private static Nock.Nock.noun N(Nock.Nock.noun a, Nock.Nock.noun b)
+        {
+            return Nock.Nock.noun.NewCell(a, b);
+        }
+
+        private static Nock.Nock.noun N(int a, Nock.Nock.noun b)
+        {
+            return Nock.Nock.noun.NewCell(N(a), b);
+        }
+
+        private static Nock.Nock.noun N(Nock.Nock.noun a, int b)
+        {
+            return Nock.Nock.noun.NewCell(a, N(b));
+        }
+
+        private static Nock.Nock.noun N(int a, int b)
+        {
+            return Nock.Nock.noun.NewCell(N(a), N(b));
+        }
+
+        private static Nock.Nock.noun N(Nock.Nock.noun a, Nock.Nock.noun b, Nock.Nock.noun c)
+        {
+            return N(a, N(b, c));
+        }
+
+        private static Nock.Nock.noun N(Nock.Nock.noun a, Nock.Nock.noun b, int c)
+        {
+            return N(a, N(b, c));
+        }
+
+        private static Nock.Nock.noun N(Nock.Nock.noun a, int b, Nock.Nock.noun c)
+        {
+            return N(a, N(b, c));
+        }
+
+        private static Nock.Nock.noun N(Nock.Nock.noun a, int b, int c)
+        {
+            return N(a, N(b, c));
+        }
+
+        private static Nock.Nock.noun N(int a, Nock.Nock.noun b, Nock.Nock.noun c)
+        {
+            return N(a, N(b, c));
+        }
+
+        private static Nock.Nock.noun N(int a, Nock.Nock.noun b, int c)
+        {
+            return N(a, N(b, c));
+        }
+
+        private static Nock.Nock.noun N(int a, int b, Nock.Nock.noun c)
+        {
+            return N(a, N(b, c));
+        }
+
+        private static Nock.Nock.noun N(int a, int b, int c)
+        {
+            return N(a, N(b, c));
+        }
+        #endregion
+
+        #region Wut test
+        [TestMethod]
+        public void WutIdentifiesCell()
+        {
+            var result = Nock.Nock.wut(N(1, 3));
+            Assert.IsInstanceOfType(result, typeof(Nock.Nock.noun.Atom));
+            Assert.AreEqual(N(0), result);
+        }
+
+        [TestMethod]
+        public void WutIdentifiesAtom()
+        {
+            var result = Nock.Nock.wut(N(1));
+            Assert.IsInstanceOfType(result, typeof(Nock.Nock.noun.Atom));
+            Assert.AreEqual(N(1), result);
+        }
+        #endregion
+
+        #region Lus test
+        [TestMethod]
+        public void LusIncrementsAtom()
+        {
+            var result = Nock.Nock.lus(N(1));
+            Assert.IsInstanceOfType(result, typeof(Nock.Nock.noun.Atom));
+            Assert.AreEqual(N(2), result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void LusThrowsOnCell()
+        {
+            Nock.Nock.lus(N(1, 2));
+        }
+        #endregion
+
+        #region Tis test
+        [TestMethod]
+        public void TisIdentifiesEquality()
+        {
+            var result = Nock.Nock.tis(N(1, 1));
+            Assert.IsInstanceOfType(result, typeof(Nock.Nock.noun.Atom));
+            Assert.AreEqual(N(0), result);
+        }
+
+        [TestMethod]
+        public void TisIdentifiesInEquality()
+        {
+            var result = Nock.Nock.tis(N(1, 2));
+            Assert.IsInstanceOfType(result, typeof(Nock.Nock.noun.Atom));
+            Assert.AreEqual(N(1), result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TisThrowsOnAtom()
+        {
+            Nock.Nock.tis(N(1));
+        }
+        #endregion
+
+        #region Fas test
+        [TestMethod]
+        public void FasSelectOne()
+        {
+            var result = Nock.Nock.fas(N(1, 2));
+            Assert.IsInstanceOfType(result, typeof(Nock.Nock.noun.Atom));
+            Assert.AreEqual(N(2), result);
+        }
+
+        [TestMethod]
+        public void FasSelectTwo()
+        {
+            var result = Nock.Nock.fas(N(2, N(1, 2)));
+            Assert.IsInstanceOfType(result, typeof(Nock.Nock.noun.Atom));
+            Assert.AreEqual(N(1), result);
+        }
+
+        [TestMethod]
+        public void FasSelectThree()
+        {
+            var result = Nock.Nock.fas(N(3, N(1, 2)));
+            Assert.IsInstanceOfType(result, typeof(Nock.Nock.noun.Atom));
+            Assert.AreEqual(N(2), result);
+        }
+
+        [TestMethod]
+        public void FasSelectLeftTree()
+        {
+            var result = Nock.Nock.fas(N(6, N(97, 2), N(1, 42, 0)));
+            Assert.IsInstanceOfType(result, typeof(Nock.Nock.noun.Atom));
+            Assert.AreEqual(N(1), result);
+        }
+
+        [TestMethod]
+        public void FasSelectRightTreeTree()
+        {
+            var result = Nock.Nock.fas(N(7, N(97, 2), N(1, 42, 0)));
+            Assert.IsInstanceOfType(result, typeof(Nock.Nock.noun.Cell));
+            Assert.AreEqual(N(42, 0), result);
+        }
+        #endregion
+
+        #region Tar test
+
+        #endregion
+
+        [TestMethod]
+        public void BasicNockTest1()
+        {
+            var result = Nock.Nock.tar(N(N(19, 42), N(0, 3)));
+
+            Assert.IsInstanceOfType(result, typeof(Nn.Atom));
+            Assert.AreEqual(42, ((Nn.Atom) result).Item);
+        }
+
+        [TestMethod]
+        public void BasicNockTest2()
+        {
+            var result = Nock.Nock.tar(N(N(132, 19), N(10, N(37, N(4, N(0, 3))))));
+
+            Assert.IsInstanceOfType(result, typeof(Nn.Atom));
+            Assert.AreEqual(20, ((Nn.Atom) result).Item);
+        }
+
+        [TestMethod]
+        public void BasicNockTest3()
+        {
+            var result = Nock.Nock.tar(N(N(N(4, 5), N(6, 14, 15)), 0, 7));
+
+            Assert.IsInstanceOfType(result, typeof(Nn.Cell));
+            Assert.AreEqual(N(14, 15), result);
+        }
+
+        [TestMethod]
+        public void BasicNockTest4()
+        {
+            var result = Nock.Nock.tar(N(41, N(1, 153, 218)));
+
+            Assert.IsInstanceOfType(result, typeof(Nn.Cell));
+            Assert.AreEqual(N(153, 218), result);
+        }
+    }
+}
